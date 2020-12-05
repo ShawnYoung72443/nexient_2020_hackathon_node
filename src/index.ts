c4d70d5401a060b434cdeb82d1ae8072f834fda5
Shawn Young <sryoung72443@gmail.com>
2020-12-05 10:50:32 -0500

    Setup as Azure Function App

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
